7dacfea56a98024ed05591a9319bbb79d32486ca
vannkaire <vk23259@students.sacredheart.school.nz>
2024-09-19 21:22:25 +0000

    updated homepage

--- Myapp.py
+++ Myapp.py
@@ -1,3 +1,17 @@
----
-my first website
----
+from bottle import run,route,template
+from datetime import datetime
+import requests
+
+@route('/')
+def index():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S:%A")
+    name = 'David'
+    response = requests.get(f'https://api.agify.io/?name={name}')
+    response = response.json()
+    age = response['age']   
+    return template('homepage', time=current_time, name = name, age=age)
+
+
+#main routine
+run(reloader=True)
